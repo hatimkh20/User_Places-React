7d62818947b2864c37c9f9b10fe42b2c79364e2c
Hatim Khuzema <hatimk51@gmail.com>
2021-11-20 19:47:03 +0500

    Userlist css added

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,22 +1,29 @@
 import React from "react";
+import Card from "../../shared/components/UIElements/Card";
 import UserItem from "./UserItem";
 
 import './UsersList.css';
 
 const UsersList = props => {
     if (props.items.length === 0) {
-        return <div className='center'>No Users Found</div>
+        return (
+            <div className='center'>
+                <Card>
+                    <h2>No Users Found</h2>
+                </Card>
+            </div>
+        )
     }
     else {
         return (
-            <ul>
+            <ul className="users-list">
                 {props.items.map(user => {
                     return (
-                        <UserItem 
-                            key={user.id} 
-                            id={user.id} 
-                            name={user.name} 
-                            image={user.image} 
+                        <UserItem
+                            key={user.id}
+                            id={user.id}
+                            name={user.name}
+                            image={user.image}
                             placeCount={user.places}
                         />
                     );
