45b539cf13261a6f43907fb0ae9ecbdcc98d006a
Hatim Khuzema <hatimk51@gmail.com>
2021-11-27 13:54:59 +0500

    Map modal added

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,29 +1,56 @@
-import React from "react";
-import Card from "../../shared/components/UIElements/Card";
+import React, { useState } from "react";
 
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import './PlaceItem.css';
 
 const PlaceItem = props => {
-    return <li className='place-item'>
-        <Card className='place-item__content'>
-            <div className='place-item__image'>
-                <img src={props.image} alt={props.title} />
-            </div>
+    const [showMap, setShowMap] = useState(false);
 
-            <div className='place-item__info'>
-                <h2>{props.title}</h2>
-                <h3>{props.address}</h3>
-                <p>{props.description}</p>
-            </div>
+    const openMapHandler = () => setShowMap(true);
 
-            <div className='place-item__actions'>
-                <button>View On Map</button>
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </Card>
+    const closeMapHandler = () => setShowMap(false);
 
-    </li>
+    return (
+        <React.Fragment>
+            <Modal 
+                show={showMap} 
+                onCancel={closeMapHandler} 
+                header={props.title} 
+                contentClass='place-item__model-content' 
+                footerClass='place-item__model-actions'
+                footer={
+                    <Button onClick={closeMapHandler}>CLOSE</Button>
+                }
+            >
+                <div className='map-container'>
+                    <Map center={props.coordinates} zoom={16} />
+                </div>
+            
+            </Modal>
+            <li className='place-item'>
+                <Card className='place-item__content'>
+                    <div className='place-item__image'>
+                        <img src={props.image} alt={props.title} />
+                    </div>
+
+                    <div className='place-item__info'>
+                        <h2>{props.title}</h2>
+                        <h3>{props.address}</h3>
+                        <p>{props.description}</p>
+                    </div>
+
+                    <div className='place-item__actions'>
+                        <Button inverse onClick={openMapHandler}>View On Map</Button>
+                        <Button to={`/places/${props.id}`}>Edit</Button>
+                        <Button danger>Delete</Button>
+                    </div>
+                </Card>
+            </li>
+        </React.Fragment> 
+    )
 }
 
 export default PlaceItem;
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { useParams } from "react-router"; 
 
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES= [
     {
-        id: 'u1',
-        title: 'Minar=e-Pakistan',
+        id: 'p1',
+        title: 'Minar-e-Pakistan',
         description: 'Minar-e-Pakistan is a national monument located in Lahore, Pakistan. The tower was built between 1960 and 1968 on the site where the All-India Muslim League passed the Lahore Resolution on 23 March 1940',
         imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/19/dd/fb/84/menar-e-pakistan.jpg',
         address: 'Minar-e-Pakistan, Circular Rd, Walled City of Lahore, Lahore, Punjab 54000, Pakistan',
@@ -16,8 +17,8 @@ const DUMMY_PLACES= [
         creatorId: 'u1'
     },
     {
-        id: 'u2',
-        title: 'Minar=e-Pakistan',
+        id: 'p2',
+        title: 'Minar-e-Pakistan',
         description: 'Minar-e-Pakistan is a national monument located in Lahore, Pakistan. The tower was built between 1960 and 1968 on the site where the All-India Muslim League passed the Lahore Resolution on 23 March 1940',
         imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/19/dd/fb/84/menar-e-pakistan.jpg',
         address: 'Minar-e-Pakistan, Circular Rd, Walled City of Lahore, Lahore, Punjab 54000, Pakistan',
@@ -31,7 +32,9 @@ const DUMMY_PLACES= [
 ]
 
 const UserPlaces = props => {
-    return <PlaceList items={DUMMY_PLACES} />
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creatorId === userId);
+    return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlaces;
