f547e13843b938be30b0b274564b22cee81ee522
Hatim Khuzema <hatimk51@gmail.com>
2022-08-15 16:05:37 +0500

    image upload and bug fixed

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import Card from "../../shared/components/UIElements/Card";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import PlaceList from "../components/PlaceList";
 
@@ -13,13 +14,29 @@ const UserPlaces = (props) => {
 
   useEffect(() => {
     const fetchPlaces = async () => {
+
       try {
-        const responseData = await sendRequest(
+        const response = await fetch(
           `http://localhost:5000/api/places/user/${userId}`
         );
-        console.log(responseData);
+        console.log("response: ", response);
+
+        if(response.status === 404){
+          console.log("Not found.")
+          return;
+       }
+
+        const responseData = await response.json()
+      
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
         setLoadedPlaces(responseData.place);
-      } catch (err) {}
+        
+      } catch (err) {
+        console.log("error: ", err)
+      }
     };
 
     fetchPlaces();
@@ -32,7 +49,6 @@ const UserPlaces = (props) => {
   };
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
@@ -41,6 +57,16 @@ const UserPlaces = (props) => {
       {!isLoading && loadedPlaces && (
         <PlaceList items={loadedPlaces} onDeletePlace={placeDeleteHandler} />
       )}
+
+      {!isLoading && !loadedPlaces && (
+        <div className='center'>
+        <Card>
+            <h2>No Places Found.</h2>
+        </Card>
+    </div>
+      )}
+      <ErrorModal error={error} onClear={clearError} />
+
     </React.Fragment>
   );
 };
